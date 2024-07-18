3c5a9435064bbf3c8c47fbabe761db28fb0210dd
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-07-19 00:43:18 +0300

    clean up

--- src/lib/components/cards/fullImageCard.svelte
+++ src/lib/components/cards/fullImageCard.svelte
@@ -4,18 +4,16 @@
   export let imageSrc = "";
 </script>
 
-<div
-  class="w-auto rounded-md shadow-xl card bg-gradient-to-tr from-base-100 to-gray-700"
->
+<div class="w-auto rounded-none card">
   <figure>
-    <img src={imageSrc} alt="card background" />
+    <img src={imageSrc} alt="card background" class="rounded-md" />
   </figure>
-  <div class="card-body">
+  <div class="px-0 card-body">
     <h2 class="text-4xl card-title">{title}</h2>
     <p class="text-md md:text-xl text-start max-w-[70ch]">
       {content}
     </p>
-    <div class="justify-end card-actions">
+    <div class="card-actions">
       <slot />
     </div>
   </div>
