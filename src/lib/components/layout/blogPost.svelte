506341f85a808752f884c5d12aef2961a6f73d48
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-07-21 23:33:20 +0300

    Add read time calculator

--- src/lib/components/layout/blogPost.svelte
+++ src/lib/components/layout/blogPost.svelte
@@ -4,16 +4,37 @@
   import Typography from "$lib/components/font/typography.svelte";
   import SectionTitle from "$lib/components/typography/sectionTitle.svelte";
   import LeftIcon from "$lib/icons/left.svelte";
+  import { afterUpdate, onMount } from "svelte";
 
   export let date = "";
   export let location = "";
   export let title = "";
   export let slides: string[] = [];
+  let slotElement: HTMLDivElement | null = null;
+  let readTime = 0;
+
+  const calculateReadingTime = () => {
+    const wordsPerMinute = 200; // Average case.
+    if (slotElement) {
+      let textLength = slotElement.innerText.split(" ").length;
+      if (textLength > 0) {
+        readTime = Math.ceil(textLength / wordsPerMinute);
+      }
+    }
+  };
+
+  const delayedCalculateReadingTime = () => {
+    setTimeout(calculateReadingTime, 0);
+  };
+
+  onMount(() => {
+    delayedCalculateReadingTime();
+  });
 </script>
 
 <div class="flex flex-col items-start w-full max-w-screen-lg text-start">
   <button class="w-8" on:click={goBack}
-    ><LeftIcon className="flex w-full " /></button
+    ><LeftIcon className="flex w-full" /></button
   >
   {#if slides.length > 1}
     <Carousel
@@ -35,15 +56,18 @@
     </div>
   {/if}
   <SectionTitle {title} withSpacing={false}>
-    <div class="flex gap-4" slot="subtitle">
+    <div class="flex flex-wrap gap-4" slot="subtitle">
       {#if date || location}
         <Typography variant="small">📅 {date}</Typography>
         <Typography variant="small">📍 {location}</Typography>
       {/if}
+      <Typography variant="small"
+        >🕒 {readTime} minute{#if readTime > 1}s{/if} read</Typography
+      >
     </div>
   </SectionTitle>
   <div class="divider" />
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full" bind:this={slotElement}>
     <slot name="info" />
   </div>
 </div>
