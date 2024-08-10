9e1c5e7c7342bc3d251f52ffda123f69822c5c28
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-08-10 15:08:29 +0300

    smaller font size

--- src/lib/components/cards/fullImageCard.svelte
+++ src/lib/components/cards/fullImageCard.svelte
@@ -18,7 +18,7 @@
     class="absolute rounded-lg bottom-0 flex justify-end w-full h-[30%] p-0 bg-gradient-to-t from-black to-transparent card-body"
   >
     <h2
-      class="self-center mb-0 md:mb-2 text-lg md:text-xl lg:text-3xl text-[#F1F1F1] card-title"
+      class="self-center mb-0 md:mb-2 text-md md:text-lg lg:text-3xl text-[#F1F1F1] card-title"
     >
       <slot />
     </h2>
