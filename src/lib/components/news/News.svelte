6a7212ac68c8c1cc7c3c306f518d20f17503cfa0
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-10-19 20:51:50 +0300

    Update news ui

--- src/lib/components/news/News.svelte
+++ src/lib/components/news/News.svelte
@@ -10,17 +10,20 @@
   class="flex flex-col items-center gap-16 px-4 py-16 md:py-24 md:px-8 bg-brand"
 >
   <SectionTitle title={"Noutăți"} variant="huge" withSpacing={false} />
-  <div class="flex flex-col w-full gap-4 lg:grid lg:grid-cols-2">
+  <div class="flex flex-col w-full md:gap-12 lg:grid lg:grid-cols-2">
     {#each actions as action}
-      <NewsCard
-        src={action.slides[0]}
-        title={action.title}
-        href={`/actiuni/${action._id}`}
-      >
-        <Typography variant="small">
-          {action.description}
-        </Typography>
-      </NewsCard>
+      <div>
+        <NewsCard
+          src={action.slides[0]}
+          title={action.title}
+          href={`/actiuni/${action._id}`}
+        >
+          <Typography variant="small">
+            {action.description}
+          </Typography>
+        </NewsCard>
+        <div class="m-0 sm:hidden divider" />
+      </div>
     {/each}
   </div>
 </section>
--- src/lib/components/news/newsCard.svelte
+++ src/lib/components/news/newsCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import SectionTitle from "$lib/components/typography/sectionTitle.svelte";
   import Carousel from "../carousel.svelte";
-  import Typography from "../font/typography.svelte";
+
   export let src = "";
   export let href = "";
   export let reversed = false;
@@ -13,13 +12,17 @@
 </script>
 
 <div
-  class={`article-grid text-start rounded-lg bg-gradient-to-r from-[#1B263B] to-[#163A47] ${fullSize ? "no-max-height" : "with-max-height"} overflow-auto relative`}
+  class={`article-grid w-full h-full text-start rounded-lg  ${fullSize ? "no-max-height" : "with-max-height"} overflow-auto relative`}
 >
   <div
     class={`image-area h-full flex ${reversed ? "col-start-4 col-end-7" : "col-start-1 col-end-4"} top-0`}
   >
     {#if src}
-      <img {src} alt="news" class="sticky object-cover w-full h-auto" />
+      <img
+        {src}
+        alt="news"
+        class="sticky object-cover w-full h-auto rounded-lg"
+      />
     {:else if slides.length > 1}
       <Carousel {slides} className="rounded-md" />
     {/if}
@@ -38,30 +41,27 @@
     class={`content-area grid p-4 ${reversed ? "col-start-1 col-end-4" : "col-start-4 col-end-7"}`}
   >
     {#if title}
-      <p class="text-xs font-bold sm:text-md md:text-lg">{title}</p>
+      <p class="text-xs font-bold md:text-lg">{title}</p>
     {/if}
     <div class="hidden pt-4 leading-relaxed md:block slot-area">
       <slot />
     </div>
-    <a class="mt-4 text-xs sm:text-sm link lg:text-md" {href}>Mai multe</a>
+    <a class="self-end mt-4 text-xs w-fit sm:text-sm link lg:text-md" {href}
+      >Mai multe</a
+    >
   </article>
 </div>
 
 <style>
-  .image-area {
-    grid-column: 1 / span 2;
-  }
   .article-grid {
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(8, 1fr);
+    grid-template-columns: repeat(8, 1fr);
   }
   .image-area {
-    grid-row: 1 / span 8;
     grid-column: 1 / span 3;
   }
   .content-area {
-    grid-row: 1 / span 8;
+    grid-column: 4 / span 5;
   }
   .title-area {
     grid-row: 1 / 2;
@@ -74,5 +74,17 @@
   }
   .no-max-height {
     max-height: auto;
+  }
+  @media (min-width: 640px) {
+    .image-area {
+      grid-row: 1 / span 8;
+      grid-column: 1 / span 3;
+    }
+    .content-area {
+      grid-row: 1 / span 8;
+    }
+    .article-grid {
+      grid-template-rows: repeat(8, 1fr);
+    }
   }
 </style>
